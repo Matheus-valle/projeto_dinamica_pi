3cdef35a917dcc1c9e18be734c64ffaaedd189ef
pedro-augusto-lemos-rodrigues <pedro.rodrigues@sptech.school>
2025-12-11 11:19:46 -0300

    banco de dados atualizado e model de medida atualizados

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -11,48 +11,48 @@ CREATE DATABASE photoTECH;
 USE photoTECH;
 
 CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
 	razao_social VARCHAR(50),
 	cnpj CHAR(14),
 	codigo_ativacao VARCHAR(50)
 );
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
 	senha VARCHAR(50),
 	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(idEmpresa)
 );
 
 CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	idAviso INT PRIMARY KEY AUTO_INCREMENT,
 	titulo VARCHAR(100),
 	descricao VARCHAR(150),
 	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(idUsuario)
 );                    
 				
 
 create table dark_room (
 /* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	idDark_room INT PRIMARY KEY AUTO_INCREMENT,
 	descricao VARCHAR(300),
 	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(idEmpresa)
 );
 
 /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
 
 create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
+	idMedida INT PRIMARY KEY AUTO_INCREMENT,
 	luminosidade INT,
 	momento DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT luminosidadePercentual
 		CHECK (luminosidade >= 0 AND luminosidade <= 100),
 	fk_dark_room INT,
-	FOREIGN KEY (fk_dark_room) REFERENCES dark_room(id)
+	FOREIGN KEY (fk_dark_room) REFERENCES dark_room(idDark_room)
 );
 
 insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
