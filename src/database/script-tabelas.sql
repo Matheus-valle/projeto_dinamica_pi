259681f0eb9081ab296095e1b9e41947b5f791d2
luisfelip2005 <gomesluisfelipe09@gmail.com>
2025-12-11 12:28:32 -0300

    arrumando script do banco

--- src/database/script-tabelas.sql
+++ src/database/script-tabelas.sql
@@ -11,48 +11,48 @@ CREATE DATABASE photoTECH;
 USE photoTECH;
 
 CREATE TABLE empresa (
-	idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	razao_social VARCHAR(50),
 	cnpj CHAR(14),
 	codigo_ativacao VARCHAR(50)
 );
 
 CREATE TABLE usuario (
-	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	nome VARCHAR(50),
 	email VARCHAR(50),
 	senha VARCHAR(50),
 	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(idEmpresa)
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
 );
 
 CREATE TABLE aviso (
-	idAviso INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	titulo VARCHAR(100),
 	descricao VARCHAR(150),
 	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(idUsuario)
+	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
 );                    
 				
 
 create table dark_room (
 /* em nossa regra de negócio, um aquario tem apenas um sensor */
-	idDark_room INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	descricao VARCHAR(300),
 	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(idEmpresa)
+	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
 );
 
 /* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
 
 create table medida (
-	idMedida INT PRIMARY KEY AUTO_INCREMENT,
+	id INT PRIMARY KEY AUTO_INCREMENT,
 	luminosidade INT,
 	momento DATETIME DEFAULT CURRENT_TIMESTAMP,
     CONSTRAINT luminosidadePercentual
 		CHECK (luminosidade >= 0 AND luminosidade <= 100),
 	fk_dark_room INT,
-	FOREIGN KEY (fk_dark_room) REFERENCES dark_room(idDark_room)
+	FOREIGN KEY (fk_dark_room) REFERENCES dark_room(id)
 );
 
 insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
@@ -60,7 +60,7 @@ insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3
 insert into dark_room (descricao, fk_empresa) values ('Sala escura dedicada para filmes', 1);
 insert into dark_room (descricao, fk_empresa) values ('Sala escura dedicada para fotos de esporte', 2);
 INSERT INTO medida (luminosidade, fk_dark_room) VALUES
-	(50, 1);
+	(50, 2);
 
 select * from empresa; 
 select * from dark_room; 
